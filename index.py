ea6e81f580825b19af913e6439a10d7fa2cc28be
ritvik1 <ritvik@zomentum.com>
2022-07-30 19:24:34 +0530

    feat: initial-commit

---
+++ index.py
@@ -0,0 +1,140 @@
+
+# Terminal program to calculate sales tax for various goods and print a receipt
+
+import re
+
+# class to define taxations for multiple edge cases
+
+
+class Taxations:
+    basicSalesTax = 10
+    importSalesTax = 5
+    exemptedGoods = ["book", "chocolate", "pills", "medicine"]
+
+    def __init__(self):
+        pass
+
+    def __isExempted(self, productName):
+        isExempted = False
+        for exemptGood in self.exemptedGoods:
+            if(productName.find(exemptGood) >= 0):
+                isExempted = True
+                break
+        return isExempted
+
+    # calculate taxation based on the parameters
+    def calculateTaxForProduct(self, product):
+        totalTaxation = 0
+        if(not self.__isExempted(product["name"])):
+            totalTaxation += (((product["price"] *
+                              product["quantity"]) * self.basicSalesTax)/100)
+            if product["isImported"]:
+                totalTaxation += ((product["price"] *
+                                  product["quantity"]) * self.importSalesTax)/100
+        else:
+            if product["isImported"]:
+                totalTaxation += ((product["price"] * product["quantity"])
+                                  * self.importSalesTax)/100
+        finalCost = round(
+            (product["price"] + totalTaxation) * product["quantity"], 2)
+        return {"salesTax": totalTaxation, "finalCost": finalCost}
+
+
+class Shop:
+    cart = []
+    receiptItems = []
+
+    def __init__(self):
+        self.cart = []
+        self.receiptItems = []
+
+    def buyItems(self):
+        numInputLines = int(input("Enter number of input lines : "))
+        lineInputs = []
+        for i in range(0, numInputLines):
+            lineInputs.append(input(f"Enter Line {i + 1} : "))
+        self.cart = lineInputs
+
+    def __decodeLine(self, inputSentence):
+        productsDetail = []
+
+        def decodeProductDetails(product):
+            price = inputSentence.lower().split(
+                f"{product['name']} at ")[1].split(" ")[0]
+
+            quantity = re.findall(
+                "[0-9]+", inputSentence.lower().partition(f"{product['name']}")[0])[-1]
+            return {
+                "name": product["name"],
+                "price": float(price),
+                "quantity": int(quantity),
+                "isImported": False
+            }
+
+        if inputSentence.lower().count("of") > 0:
+            for word in inputSentence.lower().split("of"):
+                if word.find("at") != -1:
+                    name = word.lower().lstrip().split(" at ")[0].strip()
+                    if name.find("imported") >= 0:
+                        name = name.replace("imported", "").strip()
+                    productsDetail.append({
+                        "name": name
+                    })
+        else:
+            name = " ".join(re.findall(
+                "[a-z]+", inputSentence.lower().split(" at ")[0]))
+            print(name)
+            productsDetail.append({
+                "name": name
+            })
+        productsDetail = list(map(decodeProductDetails, productsDetail))
+        for i in range(0, inputSentence.lower().count("imported")):
+            productsDetail[i].update({"isImported": True})
+
+        return productsDetail
+
+    def fetchDetailsForLineItems(self):
+        for line in self.cart:
+            self.receiptItems.append(self.__decodeLine(line))
+
+    def printCart(self):
+        for lineItem in self.cart:
+            print(lineItem)
+
+    def printReceipt(self):
+        taxation = Taxations()
+        total = 0
+        totalSalesTax = 0
+        receiptStrings = []
+        for listItem in self.receiptItems:
+            for item in listItem:
+                taxDetails = taxation.calculateTaxForProduct(item)
+                total += taxDetails["finalCost"]
+                totalSalesTax += taxDetails["salesTax"]
+        for cartItem in self.cart:
+            tempStr = cartItem
+            for listItem in self.receiptItems:
+                for item in listItem:
+                    taxDetails = taxation.calculateTaxForProduct(item)
+                    tempStr = tempStr.replace(
+                        f"{item['price']}", f"{taxDetails['finalCost']}")
+            receiptStrings.append(tempStr.strip())
+        print("\n\n")
+        for receiptString in receiptStrings:
+            print(receiptString)
+
+        print(f"Total Sales Tax : {round(totalSalesTax,2)}")
+        print(f"Total : {total}")
+        print("____________________________________________________")
+        print("Your final receipt, Thanks for shopping with US !!")
+        print("\n")
+
+
+def __init__():
+    shop = Shop()
+    shop.buyItems()
+    shop.fetchDetailsForLineItems()
+    shop.printReceipt()
+
+
+__init__()
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Problem Statement
+
+> When I purchase items I receive a receipt which lists the name of all the items and their price (including tax), finishing with the total cost of the items, and the total amounts of sales taxes paid. The rounding rules for sales tax are that for a tax rate of n%, a shelf price of p contains (np/100 rounded up to the nearest 0.05) amount of sales tax.
+
+## Problem Solving Approach
+
+**While solving the problem Regular Expressions and string formatting algorithms have been used along with string pattern matching algorithms.**
+
+## How to Run the application
+
+Based on the OS you have,
+`python index.py` should work.
+
+## Sample Input
+
+3 imported bottle of perfumeA at 27.99 2 bottle of perfumeB at 18.99\
+ 2 packet of headache pills at 9.75\
+ 1 box of imported chocolates at 11.25\
+ 1 imported box of chocolates at 10.00 1 imported bottle of perfume at 47.50\
+10 imported book at 12.49\
+ 1 music CD at 14.99\
+ 11 imported chocolate bar at 0.85\
+
+## Output Screenshot
+
+![This is an image](./terminal.png)
